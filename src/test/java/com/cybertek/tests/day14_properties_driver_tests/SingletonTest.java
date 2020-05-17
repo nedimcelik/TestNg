4cee21127a7bf45080b9d103a7b02eb1f5126dab
nedimcelik <54439547+abidinkarpuz@users.noreply.github.com>
2020-05-18 00:28:42 +0200

    add day15

--- src/test/java/com/cybertek/tests/day14_properties_driver_tests/SingletonTest.java
+++ src/test/java/com/cybertek/tests/day14_properties_driver_tests/SingletonTest.java
@@ -1,15 +1,29 @@
 package com.cybertek.tests.day14_properties_driver_tests;
+import com.cybertek.utilities.Driver;
 import org.testng.annotations.Test;
 public class SingletonTest {
     @Test
     public void test(){
         String s1 = Singleton.getInstance();
         //100 lines of code
-        System.out.println("-----------------");
+        //System.out.println("-----------------");
         String s2 = Singleton.getInstance();
-        System.out.println("-----------------");
+        //System.out.println("-----------------");
 
         System.out.println("s1 = " + s1);
         System.out.println("s2 = " + s2);
     }
+
+    @Test
+    public void test1() throws InterruptedException {
+        Driver.get().get("https://www.google.com");
+        Thread.sleep(2000);
+    }
+
+    @Test
+    public void test2(){
+        Driver.get().get("https://www.amazon.com");
+    }
+
+
 }
